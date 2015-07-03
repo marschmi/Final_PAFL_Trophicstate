f489cf605a4996fac22ad356d55a011504a2833b
Marian L. Schmidt <marschmi@umich.edu>
2015-07-03 12:22:39 -0400

    Adding data files:  otu-table, taxonomy file, metadata (individual and duplicates), rarefied 100X for invsimps and observed richness, lake profile data

---
+++ Functions_PAFL.R
@@ -0,0 +1,250 @@
+###  This is a file of functions
+####  Functions in this file include:
+# 1. multiplot:  plot multiple ggplots in one 
+# 2. makeCategories:  add categorical metadata to my dataframes
+# 3. makeCategories_dups:  add categorical metadata to my dataframe of MERGED DUPLICATES
+# 4. deSEQ:  Calculate the log2-fold ratio 
+# 5. plot_deSEQ - plot the OTU level log2-fold ratio
+# 6. plot_phylum_deSEQ - plot the PHYLUM level log2-fold ratio
+# 7. scale_reads - McMurdie & Holmes scaling to dataset for uneven sequencing depth across samples
+# 8. nacols - look for NAs in the dataframe
+
+
+
+#################################################################################### 1
+#################################################################################### 1
+# Multiple plot function is from the R-Cookbook website 
+# http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  require(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+#################################################################################### 1
+#################################################################################### 1
+
+
+
+
+#################################################################################### 2 
+#################################################################################### 2
+## This function adds the categorical metadata to a dataframe based on the sample name
+      # IMPORTANT!!!  The dataframe MUST have column named "names"
+makeCategories <- function(dataframe){ 
+  dataframe$lakenames<-substr(dataframe$names,1,3) # Create a new row called "lakenames" with first 3 letters of string
+  dataframe$limnion <- substr(dataframe$names, 4, 4) # Create a column called limnon with top or bottom 
+  dataframe$filter <- substr(dataframe$names, 6, 8) # Create a column called "filter" with pore size
+  dataframe$limnion <- as.character(dataframe$limnion)  
+  dataframe$limnion[dataframe$limnion == "E"] <- "Top"
+  dataframe$limnion[dataframe$limnion == "H"] <- "Bottom"
+  dataframe$filter <- as.character(dataframe$filter)
+  dataframe$filter[dataframe$filter == "3um"] <- "Particle"
+  dataframe$filter[dataframe$filter == ""] <- "Free"
+  dataframe$lakenames <- as.character(dataframe$lakenames)
+  dataframe$lakenames[dataframe$lakenames == "WIN"] <- "Wintergreen"
+  dataframe$lakenames[dataframe$lakenames == "SIX"] <- "Sixteen"
+  dataframe$lakenames[dataframe$lakenames == "SHE"] <- "Sherman"
+  dataframe$lakenames[dataframe$lakenames == "PAY"] <- "Payne"
+  dataframe$lakenames[dataframe$lakenames == "LON"] <- "LittleLong"
+  dataframe$lakenames[dataframe$lakenames == "LEE"] <- "Lee"
+  dataframe$lakenames[dataframe$lakenames == "GUL"] <- "Gull"
+  dataframe$lakenames[dataframe$lakenames == "BRI"] <- "Bristol"
+  dataframe$lakenames[dataframe$lakenames == "BAK"] <- "Baker"
+  dataframe$lakenames[dataframe$lakenames == "BAS"] <- "Baseline"
+  dataframe$lakenames[dataframe$lakenames == "BST"] <- "Bassett"
+  ## TO MAKE QUADRANT
+  for(i in 1:length(dataframe$limnion)){
+    dataframe$quadrant[i]<-paste(as.character(dataframe$filter[i]),as.character(dataframe$limnion[i]))}
+  ## Fix Sherman Lake
+  dataframe$quadrant[dataframe$lakenames == "Sherman" & dataframe$quadrant == "Free Top"] <- "Free Mixed"
+  dataframe$quadrant[dataframe$lakenames == "Sherman" & dataframe$quadrant == "Free Bottom"] <- "Free Mixed"
+  dataframe$quadrant[dataframe$lakenames == "Sherman" & dataframe$quadrant == "Particle Top"] <- "Particle Mixed"
+  dataframe$quadrant[dataframe$lakenames == "Sherman" & dataframe$quadrant == "Particle Bottom"] <- "Particle Mixed"
+  #Add Trophic State column by using the name of the lake
+  dataframe <- data.table(dataframe)
+  library(data.table)
+  dataframe[, trophicstate := ifelse(lakenames %in% c("Wintergreen", "Baker", "Baseline"), "Eutrophic",
+                                     ifelse(lakenames %in% c("Bassett", "Bristol", "Payne"), "Mesotrophic",
+                                            ifelse(lakenames %in% c("Sherman"), "Mixed",
+                                                   ifelse(lakenames %in% c("Gull", "Sixteen", "LittleLong", "Lee"), "Oligotrophic", NA))))]
+}
+#################################################################################### 2
+#################################################################################### 2
+
+
+
+
+#################################################################################### 3
+#################################################################################### 3
+####  This function applies the same info as the above function -> but with combined duplicates!
+######
+makeCategories_dups <- function(dataframe){ # IMPORTANT!!!  dataframe MUST have column named "names"
+  dataframe$lakenames<-substr(dataframe$names,1,3) # Create a new row called "lakenames" with first 3 letters of string
+  dataframe$limnion <- substr(dataframe$names, 4, 4) # Create a column called limnon with top or bottom 
+  dataframe$filter <- substr(dataframe$names, 5, 7) # Create a column called "filter" with pore size
+  dataframe$limnion <- as.character(dataframe$limnion)  
+  dataframe$limnion[dataframe$limnion == "E"] <- "Epilimnion"
+  dataframe$limnion[dataframe$limnion == "H"] <- "Hypolimnion"
+  dataframe$filter <- as.character(dataframe$filter)
+  dataframe$filter[dataframe$filter == "3um"] <- "Particle"
+  dataframe$filter[dataframe$filter == ""] <- "Free"
+  dataframe$lakenames <- as.character(dataframe$lakenames)
+  dataframe$lakenames[dataframe$lakenames == "WIN"] <- "Wintergreen"
+  dataframe$lakenames[dataframe$lakenames == "SIX"] <- "Sixteen"
+  dataframe$lakenames[dataframe$lakenames == "SHE"] <- "Sherman"
+  dataframe$lakenames[dataframe$lakenames == "PAY"] <- "Payne"
+  dataframe$lakenames[dataframe$lakenames == "LON"] <- "LittleLong"
+  dataframe$lakenames[dataframe$lakenames == "LEE"] <- "Lee"
+  dataframe$lakenames[dataframe$lakenames == "GUL"] <- "Gull"
+  dataframe$lakenames[dataframe$lakenames == "BRI"] <- "Bristol"
+  dataframe$lakenames[dataframe$lakenames == "BAK"] <- "Baker"
+  dataframe$lakenames[dataframe$lakenames == "BAS"] <- "Baseline"
+  dataframe$lakenames[dataframe$lakenames == "BST"] <- "Bassett"
+  ## TO MAKE QUADRANT
+  for(i in 1:length(dataframe$limnion)){
+    dataframe$quadrant[i]<-paste(as.character(dataframe$filter[i]),as.character(dataframe$limnion[i]))}
+  #Add Trophic State column by using the name of the lake
+  dataframe <- data.table(dataframe)
+  library(data.table)
+  dataframe[, trophicstate := ifelse(lakenames %in% c("Wintergreen", "Baker", "Baseline"), "Eutrophic",
+                                     ifelse(lakenames %in% c("Bassett", "Bristol", "Payne", "Sherman"), "Mesotrophic",
+                                            ifelse(lakenames %in% c("Gull", "Sixteen", "LittleLong", "Lee"), "Oligotrophic", NA)))]
+}
+#################################################################################### 3
+#################################################################################### 3
+
+
+
+
+#################################################################################### 4 + 5
+#################################################################################### 4 + 5
+#OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU OTU 
+## I wrote these 2 functions off of the following tutorial from the Phyloseq GitHub page:
+#http://joey711.github.io/phyloseq-extensions/DESeq2.html
+
+deSEQ <- function(data, valuetest){
+  data_pruned=prune_taxa(taxa_sums(data)>0,data)
+  de_data = phyloseq_to_deseq2(data_pruned, valuetest)
+  de_data2 = DESeq(de_data, test="Wald", fitType="parametric")
+  res_data = results(de_data2, cooksCutoff = FALSE)
+  alpha = 0.01
+  sig_data = res_data[which(res_data$padj < alpha), ]
+  sigtab_sherm = cbind(as(sig_data, "data.frame"), as(tax_table(data_pruned)[rownames(sig_data), ], "matrix"))
+} 
+
+
+plot_deSEQ <- function(deSEQdata, title){
+  y = tapply(deSEQdata$log2FoldChange, deSEQdata$Genus, function(x) max(x))
+  y = sort(y, TRUE)
+  deSEQdata$Genus = factor(as.character(deSEQdata$Genus), levels=names(y))
+  ggplot(deSEQdata, aes(x=Genus, y=log2FoldChange, color=Phylum)) + 
+    geom_point(size=6) +  ggtitle(title) +  
+    scale_color_manual(values = phylum.colors,name="Phylum") +
+    scale_y_continuous(breaks=seq(-15,15,1)) + 
+    theme(axis.title.x = element_text(face="bold", size=16),
+          axis.text.x = element_text(angle=30, colour = "black", vjust=1, hjust = 1, size=12),
+          axis.text.y = element_text(colour = "black", size=16),
+          axis.title.y = element_text(face="bold", size=16),
+          plot.title = element_text(face="bold", size = 20),
+          legend.title = element_text(size=12, face="bold"),
+          legend.text = element_text(size = 12),
+          strip.background = element_rect(colour="black"),
+          legend.position="right")
+}
+
+#################################################################################### 4 + 5
+#################################################################################### 4 + 5
+
+
+
+
+#################################################################################### 7
+#################################################################################### 7
+#PHYLUM PHYLUM PHYLUM PHYLUM PHYLUM PHYLUM PHYLUM PHYLUM PHYLUM PHYLUM PHYLUM PHYLUM 
+plot_phylum_deSEQ <- function(deSEQdata, title){
+  x = tapply(deSEQdata$log2FoldChange, deSEQdata$Phylum, function(x) max(x))
+  x = sort(x, TRUE)
+  deSEQdata$Phylum = factor(as.character(deSEQdata$Phylum), levels=names(x))
+  ggplot(deSEQdata, aes(x=Phylum, y=log2FoldChange, color=Phylum)) + 
+    geom_point(size=6) +  ggtitle(title) +  
+    scale_y_continuous(breaks=seq(-15,15,1)) + 
+    scale_color_manual(values = phylum.colors,name="Phylum") +
+    theme(axis.title.x = element_text(face="bold", size=16),
+          axis.text.x = element_text(angle=30, colour = "black", vjust=1, hjust = 1, size=12),
+          axis.text.y = element_text(colour = "black", size=16),
+          axis.title.y = element_text(face="bold", size=16),
+          plot.title = element_text(face="bold", size = 20),
+          legend.title = element_text(size=12, face="bold"),
+          legend.text = element_text(size = 12),
+          strip.background = element_rect(colour="black"),
+          legend.position="right")
+}
+#################################################################################### 7
+#################################################################################### 7
+
+
+
+#################################################################################### 8
+#################################################################################### 8
+## This function was written by Michelle Berry and will scale the reads to the minimum number of sample reads in the dataset
+scale_reads <- function(physeq,n){  #n is the size you want to scale to. Usually, do min(sample_sums(physeq))
+  physeq.scale <- transform_sample_counts(physeq, function(x) {(n*x/sum(x))})
+  otu_table(physeq.scale) = round(otu_table(physeq.scale))
+  physeq.scale = prune_taxa(taxa_sums(physeq.scale)>0,physeq.scale)
+  return(physeq.scale)
+}
+#################################################################################### 8
+#################################################################################### 8
+
+
+
+
+#################################################################################### 9
+#################################################################################### 9
+#Function to find NAs in a data frame
+nacols <- function(df) {
+  colnames(df)[unlist(lapply(df, function(x) any(is.na(x))))]
+}
+#################################################################################### 9
+#################################################################################### 9
+
+
---
+++ MergeReps.R
@@ -0,0 +1,110 @@
+#Load libraries
+library(phyloseq)
+library(ggplot2)
+library(ape)
+library(vegan)
+library(plyr)
+library(scales)
+library(grid)
+library(reshape2)
+library(data.table)
+library(sciplot)
+library("DESeq2")
+library(tidyr)
+library(dplyr)
+
+
+### Set the Working Directory
+setwd("~/PAFL_TrophicState/OTU_Feb18")
+
+
+### Source written functions in the file Functions.PAFL.R
+source("Functions_PAFL.R")
+
+
+### Set our theme for our plots down the road
+set_ggtheme <- theme_set(theme_bw() + 
+      theme(plot.title = element_text(face="bold", size = 20),  #Set the plot title
+        strip.text.x = element_text(size=12, face="bold"),  #Set the facet titles on x-axis 
+        strip.text.y = element_text(size=12, face="bold"),  #Set the facet titles on x-axis 
+        strip.background = element_blank(),  #Set the facet background to no background
+        axis.title.x = element_text(face="bold", size=16),  #Set the x-axis title
+        axis.title.y = element_text(face="bold", size=16),  #Set the y-axis title
+        axis.text.x = element_text(colour = "black", size=14),  #Set the x-axis labels
+        axis.text.y = element_text(colour = "black", size=14),  #Set the y-axis labels
+        legend.title = element_text(size=14, face="bold"),  #Set the legend title 
+        legend.text = element_text(size = 14),  #Set the legend text
+        legend.position="right"))  #Default the legend position to the right
+
+
+### Data import
+#sharedfile = "mothur.normalized.shared"
+sharedfile = "stability.trim.contigs.good.unique.good.filter.precluster.pick.pick.pick.an.unique_list.shared"
+taxfile = "stability.trim.contigs.good.unique.good.filter.precluster.pick.pick.pick.an.unique_list.0.03.cons.taxonomy"
+mothurdata = import_mothur(mothur_shared_file = sharedfile ,mothur_constaxonomy_file = taxfile )
+
+
+# We need to change the taxonomy names
+tax_table(mothurdata)<-cbind(tax_table(mothurdata),row.names(tax_table(mothurdata)))
+colnames(tax_table(mothurdata)) <- c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
+#View(otu_table(mothurdata))
+
+
+#We need to change some of the filenames to match our metadata
+samplenames <- sample_names(mothurdata)
+names2 <- gsub("GULL", "GUL", samplenames)
+names3 <- gsub("LONG", "LON", names2)
+sample_names(mothurdata) <- names3
+
+
+# Import metadata file and merge with mothurdata object
+#metadata <- read.csv(file = "environmental.csv")
+#for(i in 1:length(metadata$limnion)){
+#  metadata$quadrant[i]<-paste(as.character(metadata$filter[i]),as.character(metadata$limnion[i]))}
+#write.table(metadata, "metadata", sep = "\t")
+mapfile = "metadata"
+map = import_qiime_sample_data(mapfile)
+merge = merge_phyloseq(mothurdata,map)
+#View(data.frame(sample_data(merge)))
+
+
+###### Analysis ##########
+# lets look at only samples (removing blanks and mock and samples that didn't amplify)
+pruned <- prune_taxa(taxa_sums(merge) > 0, merge)
+bact_samples <-subset_taxa(pruned, Kingdom == "Bacteria")
+
+#Summary of my good_samples object
+bact_samples
+
+## Raw Sample read counts
+# Histogram of RAW sample read counts
+ggplot(data.frame(sum=sample_sums(bact_samples)),aes(sum)) + ylab("Number of Sequences per Sample") +
+  geom_histogram(colour="black",fill="dodgerblue2")  + xlab("Total Sequences") + ggtitle("Raw Sample Read Counts")
+
+# mean, max and min of sample read counts
+mins<-min(sample_sums(bact_samples))
+paste(c("The minimum sample read count is",mins))
+means<-mean(sample_sums(bact_samples))
+paste(c("The mean sample read count is",means))
+maxs<-max(sample_sums(bact_samples))
+paste(c("The max sample read count is",maxs))
+
+### Now let's scale our read counts
+good_samples <- scale_reads(physeq = bact_samples, n = min(sample_sums(bact_samples)))
+
+# Histogram of SCALED sample read counts
+ggplot(data.frame(sum=sample_sums(good_samples)),aes(sum, fill = s)) + ylab("Number of Sequences per Sample") +
+  geom_histogram(colour="black",fill="mediumorchid3") + ggtitle("McMurdie & Holme's Scaled Reads") + xlab("Total Sequences")
+
+# mean, max and min of sample read counts
+mins<-min(sample_sums(good_samples))
+paste(c("The minimum sample read count is",mins))
+means<-mean(sample_sums(good_samples))
+paste(c("The mean sample read count is",means))
+maxs<-max(sample_sums(good_samples))
+paste(c("The max sample read count is",maxs))
+
+
+
+
+
